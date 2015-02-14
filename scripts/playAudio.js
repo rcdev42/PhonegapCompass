d897e95e008552f1dc786855efae4ed8d59a7ba8
rcdev42 <rcdev42@gmail.com>
2015-02-14 12:01:25 -0800

    no message

--- scripts/playAudio.js
+++ scripts/playAudio.js
@@ -1,15 +1,31 @@
 
-
+// http://stackoverflow.com/questions/21638741/html5-audio-not-playing-in-phonegap-app-possible-to-use-media
 function playAudio(id)
 {
-    var sound = document.getElementById(id);
+    var audioElement = document.getElementById(id);
+    var url = audioElement.getAttribute('src');
 
-    if (!sound.paused)
-    {
-        sound.pause();
-        sound.currentTime = 0.0;
-    }
+    var my_media = new Media(url,
+        // success callback
+        function () { console.log("playAudio():Audio Success"); },
+        // error callback
+        function (err) { console.log("playAudio():Audio Error: " + err); }
+    );
 
-    sound.currentTime = 0.1;
-    sound.play();
+    // Play audio
+    my_media.play();
 }
+
+//    function playAudio(id)
+//    {
+//        var sound = document.getElementById(id);
+//
+//        if (!sound.paused)
+//        {
+//            sound.pause();
+//            sound.currentTime = 0.0;
+//        }
+//
+//        sound.currentTime = 0.1;
+//        sound.play();
+//    }
